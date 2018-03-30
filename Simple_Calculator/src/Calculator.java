ad03ca0087db50a166d319e40efe9c071d6f23f1
USER <USER@test.com>
2018-03-30 14:10:13 +0900

    Calculator is initialized 추가

--- Simple_Calculator/src/Calculator.java
+++ Simple_Calculator/src/Calculator.java
@@ -6,6 +6,7 @@ public class Calculator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Calculator is initialized..");
+		System.out.println("Calculator is initialized..");
 	}
 
 }
